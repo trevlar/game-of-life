db2c60607aaea645d7762de617d6519bc69e17c6
Trevor Carlston <trevorcarlston@Trevors-MacBook-Pro.local>
2023-12-16 14:07:35 -0700

    Update board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import GameControls from './features/GameControls';
+import GameBoard from './features/GameBoard';
 
-function App() {
+function GameOfLife() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Conways Game of Life
       </header>
+      <GameControls />
+      <GameBoard />
     </div>
   );
 }
---
+++ src/features/GameBoard/GameBoard.tsx
@@ -0,0 +1,5 @@
+function GameBoard() {
+    return (
+        <div></div>
+    )
+}
